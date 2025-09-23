b3e9bad0fbc2e8369ceeaeb2d999d4256c4b47b8
JuanG <juandavidgome336@gmail.com>
2025-09-23 10:38:30 -0500

    new

---
+++ frontend/src/components/Header/header.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { useState } from 'react'
+import './header.css'
+const header = () => {
+  return (
+    <>
+    
+    <header>
+
+        <h1>Bienvenido</h1>
+    </header>
+    
+    
+    </>
+  )
+}
+
+export default header
